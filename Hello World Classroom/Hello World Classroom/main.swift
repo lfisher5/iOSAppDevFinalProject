1c26c56032535f2426ce8d8ac13d7afd4c3fb725
lfisher5 <lfisher5@zagmail.gonzaga.edu>
2020-09-03 15:32:09 -0700

    Added another print statement

---
+++ Hello World Classroom/Hello World Classroom/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Hello World Classroom
+//
+//  Created by Lauren Fisher on 9/3/20.
+//  Copyright © 2020 Lauren Fisher. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+print("Another print statement")
+
